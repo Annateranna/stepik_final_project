1b418d260a0162083e0fff8bc63af91dbeda7f7f
Annateranna <annateranna@gmail.com>
2022-04-11 15:11:14 +0300

    Урок: поиск ошибочной ссылки

--- test_product_page.py
+++ test_product_page.py
@@ -1,16 +1,29 @@
+import pytest
+
 from .pages.main_page import MainPage
 import time
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+@pytest.mark.parametrize('link',
+                             ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                              pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                              "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page = MainPage(browser, link)
     page.open()                      # открываем страницу
     # выполняем метод страницы — переходим на страницу логина
     product_page = ProductPage(browser, browser.current_url)
-    time.sleep(5)
+    time.sleep(2)
     product_page.put_to_basket()
     product_page.should_be_in_basket()
     product_page.the_price_should_be_equal()
